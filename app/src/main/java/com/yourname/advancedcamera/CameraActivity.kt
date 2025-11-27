9c009d898897ed21b3658f0a9187dd17b82e7e74
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-11-27 21:42:39 +0530

    Update CameraActivity.kt

--- app/src/main/java/com/yourname/advancedcamera/CameraActivity.kt
+++ app/src/main/java/com/yourname/advancedcamera/CameraActivity.kt
@@ -923,7 +923,7 @@ class CameraActivity : AppCompatActivity() {
         }
     }
     
-    internal class CompareSizesByArea : Comparator<Size> {
+        internal class CompareSizesByArea : Comparator<Size> {
         override fun compare(lhs: Size, rhs: Size): Int {
             return java.lang.Long.signum(
                 lhs.width.toLong() * lhs.height - rhs.width.toLong() * rhs.height
